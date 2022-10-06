881b2875e9af9e6bfb99cb98f9a7eac0472613d8
Huaxuan Yang <yanghuaxuan@gmail.com>
2022-10-05 21:55:07 -0700

    Remove unused files

--- Contents/BPCPlayer.cs
+++ Contents/BPCPlayer.cs
@@ -2,10 +2,13 @@
 
 namespace BPConstructs.Contents
 {
+    /**
+     * Tracks whether player is in architectMode
+     */
     internal class BPCPlayer : ModPlayer
     {
         private bool _architectMode = false;
-        
+
         public bool architectMode
         {
             get => _architectMode;
