47494c006d8a1f867739fd1172c6fb688dc05a2d
Huaxuan Yang <yanghuaxuan@gmail.com>
2022-10-23 14:54:13 -0700

    Remove CreateThumbnail in BPContainer fr this time

--- Contents/BPContainerPanel.cs
+++ Contents/BPContainerPanel.cs
@@ -28,31 +28,6 @@ namespace BPConstructs.Contents
             set { _tiles = value; }
         }
 
-        // private void CreateThumbnail(Tile[,] tiles, Vector2 startPos, SpriteBatch sb)
-        // {
-        //     int thumbnailWidth = 100;
-        //     int thumbnailHeight = 100;
-        //     int tileWidth = tiles.GetLength(0);
-        //     int tileHeight = tiles.GetLength(1);
-        //     float scale = 1.0F;
-        //     Vector2 offset = new Vector2();
-
-        //     if (tileWidth > thumbnailWidth || tileHeight > thumbnailHeight)
-        //     {
-        //         if (tileHeight > tileWidth)
-        //         {
-        //             scale = (float)thumbnailWidth / tileHeight;
-        //             offset.X = (thumbnailWidth - tileWidth * scale);
-        //         }
-        //         else
-        //         {
-        //             scale = (float)thumbnailWidth / tileWidth;
-        //             offset.Y = (thumbnailHeight - tileHeight * scale) / 2;
-        //         }
-        //     }
-
-        //     CopyModeUI.DrawPreview(sb, tiles, startPos, 0.2f);
-        // }
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.End();
